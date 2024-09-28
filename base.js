4b94715fd752377b42e2f2fa5dbe3a3f4c4dc4df
charan6616 <charan6616@gmail.com>
2024-09-28 21:56:33 +0530

    day 2 28/09/24

---
+++ base.js
@@ -0,0 +1,8 @@
+function cv(){
+    document.getElementById("open").style.width="100px";
+    document.getElementById("tap").style.marginLeft="100px";
+}
+function cvclose(){
+    document.getElementById("open").style.width="0";
+    document.getElementById("tap").style.marginLeft="0";
+}
